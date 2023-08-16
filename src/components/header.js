53c5178d09600375f0eb174ea34ab6ab6cd16951
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-08-17 02:50:19 +0700

    Creating responsive navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,14 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
 import style from "@/components/navbar.module.css";
 import Image from "next/image";
+import burger from "../../public/burger_icon.svg";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <>
       <div className={style.donasi}>
@@ -18,89 +24,23 @@ export default function Navbar() {
           </a>
         </div>
 
-        <div className={style.navigation}>
-          <a href="/">Beranda</a>
-          <a href="/Edukasi">Edukasi</a>
-          <a href="/TentangKami">Tentang</a>
-          <a href="/FAQ">FAQ</a>
-          <a href="/Kegiatan">Kegiatan</a>
-          <a href="/Tim">Tim</a>
+        <button
+          type="button"
+          onClick={() => setIsActive(!isActive)}
+          className={style.burger}
+        >
+          <Image src={burger} alt="burger" width={60} height={60} />
+        </button>
+
+        <div className={`${style.navigation} ${isActive ? "flex" : ""}`}>
+          <Link href="/">Beranda</Link>
+          <Link href="/Edukasi">Edukasi</Link>
+          <Link href="/TentangKami">Tentang</Link>
+          <Link href="/FAQ">FAQ</Link>
+          <Link href="/Kegiatan">Kegiatan</Link>
+          <Link href="/Tim">Tim</Link>
         </div>
       </div>
     </>
   );
-
-  // return (
-  //   <>
-  //     <div class="donasi_container">
-  //       <h1>
-  //         <a class="donasi" href="#">
-  //           Donasi Klik Disini
-  //         </a>
-  //       </h1>
-  //     </div>
-
-  //     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  //       <div class="container-fluid">
-  //         <a class="logo" href="/">
-  //           <Image src="/Logo Potads.png" alt="logo" width={80} height={80} />
-  //         </a>
-  //         <button
-  //           class="navbar-toggler"
-  //           type="button"
-  //           data-bs-toggle="collapse"
-  //           data-bs-target="#navbarSupportedContent"
-  //           aria-controls="navbarSupportedContent"
-  //           aria-expanded="false"
-  //           aria-label="Toggle navigation"
-  //         >
-  //           <span class="navbar-toggler-icon"></span>
-  //         </button>
-  //         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  //           <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
-  //             <li class="nav-item">
-  //               <a
-  //                 class="nav-link active"
-  //                 aria-current="page"
-  //                 href="index.html"
-  //               >
-  //                 Beranda
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="/Edukasi/">
-  //                 Edukasi
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="/TentangKami/">
-  //                 Tentang Kami
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="/FAQ/">
-  //                 FAQ
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="/Kegiatan/">
-  //                 Kegiatan
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="/Tim/">
-  //                 Tim
-  //               </a>
-  //             </li>
-  //             <li class="nav-item">
-  //               <a class="nav-link" href="/Kontak/">
-  //                 Kontak
-  //               </a>
-  //             </li>
-  //           </ul>
-  //         </div>
-  //       </div>
-  //     </nav>
-  //   </>
-  // );
 }
