de09736a53da5696b24e31ccf9233f347ed7d820
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-08-31 20:35:20 +0700

    Updating join

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -10,15 +10,34 @@ export default function Join() {
         </div>
 
         <div className={`${style.container_content}`}>
-          <Link href="/" className={`${style.button_volunteer}`}>
-            Relawan
-          </Link>
-          <Link href="/" className={`${style.button_volunteer}`}>
-            Relawan
-          </Link>
-          <Link href="/" className={`${style.button_volunteer}`}>
-            Relawan
-          </Link>
+          <div className={`${style.daftar_anak}`}>
+            <h1>Bergabung dengan PIK POTADS sesuai dengan umur anak anda</h1>
+
+            <div className={`${style.link_list}`}>
+              <Link
+                href="https://bit.ly/DataAnggotaPotadsJabar0-6thn"
+                className={`${style.button_volunteer}`}
+              >
+                0 - 6 Tahun
+              </Link>
+              <Link
+                href="https://bit.ly/DataAnggotaPotadsJabar6-15thn"
+                className={`${style.button_volunteer}`}
+              >
+                7 - 15 Tahun
+              </Link>
+              <Link
+                href="https://bit.ly/DataAnggotaPotadsJabar15thplus"
+                className={`${style.button_volunteer}`}
+              >
+                Lebih dari 15 Tahun
+              </Link>
+            </div>
+          </div>
+
+          <div className={`${style.other_join}`}>
+            <Link href="/">Relawan</Link>
+          </div>
         </div>
       </div>
     </>
