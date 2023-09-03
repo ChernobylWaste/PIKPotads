29598ae3319aa3395f78c64eaf78d6d6f684c352
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-09-03 20:06:56 +0700

    Fixing font and adding relawan button

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -5,7 +5,17 @@ export default function Join() {
   return (
     <>
       <div className={`${style.full_container}`}>
-        <h1 className={`${style.container_title}`}>Join us</h1>
+        <div className={`${style.container_head}`}>
+          <h1 className={`${style.container_title}`}>Join us</h1>
+
+          <Link
+            className={`${style.join_relawan}`}
+            href="https://docs.google.com/forms/d/1AHAxrIM-oR2cMBkNttrPs4ESx94iCoikQqga5oaiIwE/viewform?edit_requested=true"
+            target="_blank"
+          >
+            Relawan
+          </Link>
+        </div>
 
         <div className={`${style.container_content}`}>
           <div className={`${style.daftar_anak}`}>
@@ -15,26 +25,25 @@ export default function Join() {
               <Link
                 href="https://bit.ly/DataAnggotaPotadsJabar0-6thn"
                 className={`${style.button_volunteer}`}
+                target="_blank"
               >
                 0 - 6 Tahun
               </Link>
               <Link
                 href="https://bit.ly/DataAnggotaPotadsJabar6-15thn"
                 className={`${style.button_volunteer}`}
+                target="_blank"
               >
                 7 - 15 Tahun
               </Link>
               <Link
                 href="https://bit.ly/DataAnggotaPotadsJabar15thplus"
                 className={`${style.button_volunteer}`}
+                target="_blank"
               >
                 Lebih dari 15 Tahun
               </Link>
             </div>
-          </div>
-
-          <div className={`${style.other_join}`}>
-            <Link href="/">Relawan</Link>
           </div>
         </div>
       </div>
